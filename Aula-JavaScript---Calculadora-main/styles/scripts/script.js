fc090d2aae6c07ee59453421dcc3e965129d546a
vitinn108 <alvesvictor3511@gmail.com>
2025-08-26 12:20:04 -0300

    Add files via upload

---
+++ Aula-JavaScript---Calculadora-main/styles/scripts/script.js
@@ -0,0 +1,15 @@
+const screen = document. getElementById('answer');
+
+const buttons = document.querySelectorAll('.calc-btn');
+
+const historyBtn = document.getElementById('history-btn');
+
+const historypanel = document.getElementById('history-panel');
+
+const historylist = document.getElementById('history-list');
+
+const closeHistory = document.getElementById('close-history');
+
+const clearhistory = document.getElementById('clear-history');
+
+const themeToggle = document.getElementById('theme-toggle');
